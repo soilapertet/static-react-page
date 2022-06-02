1d2308237560168b134bc9a5d7d519b671d4eac1
soilapertet <nicolepertet@gmail.com>
2022-06-02 10:31:53 +0300

    Correct errors in HTML and JS file and render page content

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
+// Initialise the JSX content you want to render and pass it to the render() method
 const rootContent = (
   <div>
-    <img src="img/React_(web_framework)-Logo.wine.png" alt="React logo" class="logo"/>
+    <img src="img/React_(web_framework)-Logo.wine.png" alt="React logo" width="100"/>
     <h1>Fun facts about React</h1>
-      <ul class="react-facts">
-        <li class="react-fact">Was first released in 2013</li>
-        <li class="react-fact">Was originally created by Jordan Walke</li>
-        <li class="react-fact">Has well over 100K stars on Github</li>
-        <li class="react-fact">Is maintained by Facebook</li>
-        <li class="react-fact">Powers thousands of enterprise apps, including mobile apps</li>
+      <ul className="react-facts">
+        <li className="react-fact">Was first released in 2013</li>
+        <li className="react-fact">Was originally created by Jordan Walke</li>
+        <li className="react-fact">Has well over 100K stars on Github</li>
+        <li className="react-fact">Is maintained by Facebook</li>
+        <li className="react-fact">Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
   </div>
 );
+
+//Initiliase where you want to render the JSX
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(rootContent);
