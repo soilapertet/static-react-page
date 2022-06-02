8a2a407fda6055cd3af787cabda7c0b5ef5f6183
soilapertet <nicolepertet@gmail.com>
2022-06-02 10:00:47 +0300

    Initialise content for the page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const rootContent = (
+  <div>
+    <img src="img/React_(web_framework)-Logo.wine.png" alt="React logo" class="logo"/>
+    <h1>Fun facts about React</h1>
+      <ul class="react-facts">
+        <li class="react-fact">Was first released in 2013</li>
+        <li class="react-fact">Was originally created by Jordan Walke</li>
+        <li class="react-fact">Has well over 100K stars on Github</li>
+        <li class="react-fact">Is maintained by Facebook</li>
+        <li class="react-fact">Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+  </div>
+);
